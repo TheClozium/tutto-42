2092246cdb15abc4af9eb03a94d9b8e41dc2e202
TheClozium <145875004+TheClozium@users.noreply.github.com>
2023-10-23 15:09:38 +0200

    Add files via upload

---
+++ exam/ft_range/ft_rangex.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rangex.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: socloza <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/17 17:29:23 by socloza           #+#    #+#             */
+/*   Updated: 2023/10/17 17:36:11 by socloza          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <stdio.h>
+#include <stdlib.h>
+
+int	*ft_range(int start, int end)
+{
+	int	i = 0;
+	int	*t;
+
+	if (start > end)
+	t = (int *)malloc(sizeof(*t) * (start - end + 1));
+	else
+	t = (int *)malloc(sizeof(*t) * (end - start + 1));
+	if (t == NULL)
+	{
+		return (NULL);
+	}
+	while (start != end)
+	{
+		t[i] = start;
+		i++;
+		if (start < end)
+			start++;
+		else
+			start--;
+	}
+	t[i] = end;
+	return (t);
+}
+int		main(void)
+{
+	int		i;
+	int		*tab;
+	
+	i = 0;
+	tab = ft_range(1, 3);
+	while (i < 3)	
+	{
+		printf("%d\n", tab[i]);
+		i++;
+	}
+	free(tab);
+}
